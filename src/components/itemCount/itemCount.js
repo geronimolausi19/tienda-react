6a11189abf83bc78c3c803e3abf9b8e8c0b22137
geronimolausi19 <77115866+geronimolausi19@users.noreply.github.com>
2021-05-10 23:00:26 -0300

    commitclase9

--- src/components/itemCount/itemCount.js
+++ src/components/itemCount/itemCount.js
@@ -1,34 +1,43 @@
 import React, { useState } from "react"
-export const ItemCount = ({stock}) => {
+import {Link} from "react-router-dom"
+
+export const ItemCount = ({stock, initial, onAdd}) => {
    
 
 
-const [count, setCount] = useState(1)
+const [count, setCount] = useState(initial)
 
 const Sumar = () => {         
     if(count < stock) {     
       setCount(count + 1)   
-      console.log(count)  
+     
     }
 }
 
 const Restar = () => {        
     if(count > 1) {        
       setCount(count - 1)      
-      console.log(count)
+      
     }
 }
   
 
     
-return (            
+return (   
+  <div>        
     <div>
         <button onClick={Sumar} > + </button>
-        <p>Cantidad de items en el carrito {count}</p>
+        <p>Cantidad de items en el carrito {count} {console.log(count)}</p>
         <button onClick={Restar} > - </button>
+        </div>
+<br/>
+<div>
+        <button >Agregar al carrito</button>
+    <Link onClick={ () => onAdd(count) } >añadir al carrito</Link>
     </div>
+     </div>
     )
-   }
+}
   
 
  
